4f207c20a8b9daa58cab135a7be577cbdc32e6d3
Aditi Bhavsar <97657912+Aditib611@users.noreply.github.com>
2024-01-23 18:44:38 +0530

    Updated

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
-import path from "path";
-import {fileURLToPath} from 'url';
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import path from "path";
+import {fileURLToPath} from 'url';
 //configure env
 dotenv.config();
 
@@ -16,8 +16,8 @@ dotenv.config();
 connectDB();
 
 //esmodule fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename)
 //rest object
 const app = express();
 
